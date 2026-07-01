606a23142e36e5e2202f64b3af26bff15fa011c3
SouuYe07 <iesouaguila07@gmail.com>
2026-07-01 09:25:46 +0800

    Exer 2

--- Chapter 3/Book.java
+++ Chapter 3/Book.java
@@ -5,7 +5,35 @@ public class Book {
     private boolean fiction;
 
     
-    public getAuthor(){
+    public Author getAuthor(){
         return this.author;
+    }
+
+    public void setAuthor(Author newAuthor){
+        this.author = newAuthor; 
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public int getNoOfPages(){
+        return this.noOfPages;
+    }
+
+    public void setNoOfPages(int newOfNoPages){
+        this.noOfPages = newOfNoPages;
+    }
+
+    public boolean isFiction(){
+        return this.fiction;
+    }
+
+    public void setIsFiction(boolean bool){
+        this.fiction = bool;
     }
 }
